8b74746dfdc64b79130791daa5d1287e09baee9f
anuswantp <anuswantp1998@gmail.com>
2021-05-15 03:31:35 +0530

    Google reviews

--- src/pages/testimonials/index.js
+++ src/pages/testimonials/index.js
@@ -1,8 +1,10 @@
 import React,{useEffect,useState} from 'react'
-import { Center,Text } from '@chakra-ui/layout'
+import { Box, Center,Text } from '@chakra-ui/layout'
 import { Testimonials } from '../home'
 import { getTestimonials } from '../../../services/api/testimonials.api'
 import { Spinner } from "@chakra-ui/react";
+import {FcGoogle} from "react-icons/fc"
+import { Col, Row } from 'react-bootstrap';
 function TestimonialPage() {
     const [reviews,setReviews]=useState([])
     const [loading,setLoading]=useState(true)
@@ -20,11 +22,30 @@ function TestimonialPage() {
     return (
             <div style={{padding:"20px", minHeight:"75vh", width:"100%"}}>
              <>
-            <Center>
+            <Row>
+                <Col md={10}>
+            <Center ml={[0,"20%"]}>
             <Text as="h1" textAlign="center" color="purple.600" fontSize="2em" fontWeight="600">
                 All Testimonials
             </Text>
             </Center>
+            </Col>
+            <Col md={2}>
+            <Box style={{
+                display:"flex",
+                alignItems:"center",
+                justifyContent:"center"
+                // float:"right"
+            }}>
+            <FcGoogle size="2em"/>
+            <a
+            style={{color:"#6f42c1",marginLeft:"5px",fontSize:"14px"}}
+            target="_blank"
+            href="https://www.google.com/search?hl=en-IN&gl=in&q=LearnWithProjects.com,+La+Gardenia,+140/lg+Pragathi+Enclave+Colony,+Miyapur,+Telangana+500049&ludocid=6357780336637525232&lsig=AB86z5WmJt7gpsjLPAXH_hUCPbWe#lrd=0x3bcb93a78eec355d:0x583b5f87fd3e3cf0,1"
+            >Read all Google Reviews</a>
+            </Box>
+            </Col>
+            </Row>
             {
                 loading?
                <Center>
