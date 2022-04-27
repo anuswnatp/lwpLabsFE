850e1f58de3b56dbf23776580d0389ad0179bcba
Anuswant Pandey <54389204+anuswnatp@users.noreply.github.com>
2022-04-27 14:07:53 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         // },
         'gatsby-transformer-sharp',
         'gatsby-plugin-sharp',
-        'gatsby-plugin-offline',
+        'gatsby-plugin-remove-serviceworker',
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
