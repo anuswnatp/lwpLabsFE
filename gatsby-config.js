dc45991aa2bf78dae590e732a507d0492eea1733
Anuswant Pandey <54389204+anuswnatp@users.noreply.github.com>
2022-04-27 14:07:02 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         //         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
         //     },
         // },
-        'gatsby-plugin-offline',
+        'gatsby-plugin-remove-serviceworker',
     ],
     proxy: {
         prefix: "/api",
